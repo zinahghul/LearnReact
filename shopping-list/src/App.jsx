8a407f41980b99d2e8c98986dfd48d5af61c24e6
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-11-18 00:13:05 -0500

    App.jsx with steps

---
+++ shopping-list/src/App.jsx
@@ -0,0 +1,41 @@
+// Step 4. Import React and useState
+
+// Step 5. Import the App.css file
+
+// Step 6. Create a functional component called App
+
+    // Step 8a. Initialize useState state variables
+
+    
+    // Step 8b. Create arrow function 'handleInputChange to store the current user input
+   
+    
+    // Step 9. Create arrow function 'handleAddTask' to add tasks to the list
+    
+
+    // Step 11. Create arrow function 'handleDeleteTask' with parameter 'index' to delete tasks
+   
+        // Step 11a. Create a new list of items to keep
+
+        // Step 11b. Set the list to our new list
+
+    // Step 7. Return the JSX for the App component
+
+        // Step 7a. Create a div with className 'list' and create a site header
+        
+            {/* Step 7b. Create a div with className 'text' and create an input field with:
+            type 'text' 
+            value inputValue
+            onChange set to handleInputChange
+            placeholder to any placeholder text
+            */}
+           
+           
+            {/* Step 10. Display the shopping list array */}
+            
+            
+                {/* Step 10a. Iterate over every task and retrieve its index*/}
+                
+
+
+
